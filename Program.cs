df645abf247671e96b5d8559897ab7718af2c94e
Erin Millard <contact@erin.id.au>
2022-10-07 18:17:05 +1000

    Exit gracefully if no args are passed

--- Program.cs
+++ Program.cs
@@ -4,8 +4,17 @@ using Ezzatron.Hello;
 
 class Program
 {
-  static void Main(string[] args)
+  static int Main(string[] args)
   {
+    if (args.Length < 1)
+    {
+      Console.Error.WriteLine($"usage: {System.AppDomain.CurrentDomain.FriendlyName} <name>");
+
+      return 1;
+    }
+
     Console.WriteLine(Greeter.Greet(args[0]));
+
+    return 0;
   }
 }
